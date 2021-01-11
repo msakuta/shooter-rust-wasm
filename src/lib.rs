5d4b8b75826eb023be10868cebd8eb7772967cfa
msakuta <masahiro.sakuta@gmail.com>
2021-01-11 21:06:16 +0900

    First attenmpt to texture mapping

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader, HtmlImageElement, WebGlTexture};
+use web_sys::{HtmlImageElement, WebGlProgram, WebGlRenderingContext, WebGlShader, WebGlTexture};
 
 macro_rules! console_log {
     ($fmt:expr, $($arg1:expr),*) => {
@@ -18,9 +19,20 @@ extern "C" {
     pub(crate) fn log(s: &str);
 }
 
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
+    let window = window();
+    let document = window.document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
 
@@ -66,13 +78,13 @@ pub fn start() -> Result<(), JsValue> {
 
     let loc = context.get_uniform_location(&program, "texture");
     console_log!("loc: {}", loc.is_some());
-    let texture = load_texture(&context, "");
+    let texture = load_texture(&context, "./assets/enemy.png")?;
 
     // Tell WebGL we want to affect texture unit 0
     context.active_texture(WebGlRenderingContext::TEXTURE0);
 
     // Bind the texture to texture unit 0
-    context.bind_texture(WebGlRenderingContext::TEXTURE_2D, texture.as_ref());
+    context.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&*texture));
     context.uniform1i(loc.as_ref(), 0);
 
     let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
@@ -101,14 +113,52 @@ pub fn start() -> Result<(), JsValue> {
     context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
     context.enable_vertex_attrib_array(0);
 
-    context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+    context.clear_color(0.0, 0.0, 0.5, 1.0);
 
-    context.draw_arrays(
-        WebGlRenderingContext::TRIANGLES,
-        0,
-        (vertices.len() / 3) as i32,
-    );
+    // Here we want to call `requestAnimationFrame` in a loop, but only a fixed
+    // number of times. After it's done we want all our resources cleaned up. To
+    // achieve this we're using an `Rc`. The `Rc` will eventually store the
+    // closure we want to execute on each frame, but to start out it contains
+    // `None`.
+    //
+    // After the `Rc` is made we'll actually create the closure, and the closure
+    // will reference one of the `Rc` instances. The other `Rc` reference is
+    // used to store the closure, request the first frame, and then is dropped
+    // by this function.
+    //
+    // Inside the closure we've got a persistent `Rc` reference, which we use
+    // for all future iterations of the loop
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    let mut i = 0;
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        if i > 300 {
+            console_log!("All done!");
+
+            // Drop our handle to this closure so that it will get cleaned
+            // up once we return.
+            let _ = f.borrow_mut().take();
+            return;
+        }
+
+        // Set the body's text content to how many times this
+        // requestAnimationFrame callback has fired.
+        i += 1;
+        console_log!("requestAnimationFrame has been called {} times.", i);
+        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+        context.draw_arrays(
+            WebGlRenderingContext::TRIANGLES,
+            0,
+            (vertices.len() / 3) as i32,
+        );
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
 }
 
@@ -162,15 +212,14 @@ pub fn link_program(
     }
 }
 
-
 //
 // Initialize a texture and load an image.
 // When the image finished loading copy it into the texture.
 //
-fn load_texture(gl: &WebGlRenderingContext, url: &str) -> Option<WebGlTexture> {
-    let texture = gl.create_texture();
-    gl.bind_texture(WebGlRenderingContext::TEXTURE_2D, texture.as_ref());
-  
+fn load_texture(gl: &WebGlRenderingContext, url: &str) -> Result<Rc<WebGlTexture>, JsValue> {
+    let texture = Rc::new(gl.create_texture().unwrap());
+    gl.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&*texture));
+
     // Because images have to be downloaded over the internet
     // they might take a moment until they are ready.
     // Until then put a single pixel in the texture so we can
@@ -178,50 +227,95 @@ fn load_texture(gl: &WebGlRenderingContext, url: &str) -> Option<WebGlTexture> {
     // we'll update the texture with the contents of the image.
     let level = 0;
     let internal_format = WebGlRenderingContext::RGBA as i32;
-    let width = 2;
-    let height = 2;
+    let width = 1;
+    let height = 1;
     let border = 0;
     let src_format = WebGlRenderingContext::RGBA;
     let src_type = WebGlRenderingContext::UNSIGNED_BYTE;
-    let pixel = [
-        0u8, 255, 255, 255,
-        255, 0, 0, 255,
-        0, 255, 0, 255,
-        255,255,255,255,
-    ];
+    let pixel = [0u8, 255, 255, 255];
     gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-        WebGlRenderingContext::TEXTURE_2D, level, internal_format,
-                  width, height, border, src_format, src_type,
-                  Some(&pixel)).unwrap();
-    gl.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_WRAP_S, WebGlRenderingContext::REPEAT as i32);
-    gl.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_WRAP_T, WebGlRenderingContext::REPEAT as i32);
-    gl.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_MIN_FILTER, WebGlRenderingContext::LINEAR as i32);
+        WebGlRenderingContext::TEXTURE_2D,
+        level,
+        internal_format,
+        width,
+        height,
+        border,
+        src_format,
+        src_type,
+        Some(&pixel),
+    )
+    .unwrap();
+    gl.tex_parameteri(
+        WebGlRenderingContext::TEXTURE_2D,
+        WebGlRenderingContext::TEXTURE_WRAP_S,
+        WebGlRenderingContext::REPEAT as i32,
+    );
+    gl.tex_parameteri(
+        WebGlRenderingContext::TEXTURE_2D,
+        WebGlRenderingContext::TEXTURE_WRAP_T,
+        WebGlRenderingContext::REPEAT as i32,
+    );
+    gl.tex_parameteri(
+        WebGlRenderingContext::TEXTURE_2D,
+        WebGlRenderingContext::TEXTURE_MIN_FILTER,
+        WebGlRenderingContext::LINEAR as i32,
+    );
 
-    let image = HtmlImageElement::new().unwrap();
-    // image.set_onload(|| {
-    //   gl.bind_texture(WebGlRenderingContext::TEXTURE_2D, texture);
-    //   gl.texImage2D(WebGlRenderingContext::TEXTURE_2D, level, internal_format,
-    //                 srcFormat, srcType, image);
-  
-    //   // WebGL1 has different requirements for power of 2 images
-    //   // vs non power of 2 images so check if the image is a
-    //   // power of 2 in both dimensions.
-    //   if (is_power_of_2(image.width) && is_power_of_2(image.height)) {
-    //      // Yes, it's a power of 2. Generate mips.
-    //      gl.generateMipmap(gl.TEXTURE_2D);
-    //   } else {
-    //      // No, it's not a power of 2. Turn off mips and set
-    //      // wrapping to clamp to edge
-    //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    //   }
-    // });
+    let image = Rc::new(HtmlImageElement::new().unwrap());
+    let url_str = url.to_owned();
+    let image_clone = image.clone();
+    let texture_clone = texture.clone();
+    let callback = Closure::wrap(Box::new(move || {
+        console_log!("loaded image: {}", url_str);
+        // web_sys::console::log_1(Date::new_0().to_locale_string("en-GB", &JsValue::undefined()));
+
+        let f = || -> Result<(), JsValue> {
+            let window = web_sys::window().unwrap();
+            let document = window.document().unwrap();
+            let canvas = document.get_element_by_id("canvas").unwrap();
+            let canvas: web_sys::HtmlCanvasElement =
+                canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+            let context = canvas
+                .get_context("webgl")?
+                .unwrap()
+                .dyn_into::<WebGlRenderingContext>()?;
+
+            context.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&*texture_clone));
+            context.tex_image_2d_with_u32_and_u32_and_image(
+                WebGlRenderingContext::TEXTURE_2D,
+                level,
+                internal_format,
+                src_format,
+                src_type,
+                &image_clone,
+            );
+            Ok(())
+        };
+
+        f().ok();
+
+        //   // WebGL1 has different requirements for power of 2 images
+        //   // vs non power of 2 images so check if the image is a
+        //   // power of 2 in both dimensions.
+        //   if (is_power_of_2(image.width) && is_power_of_2(image.height)) {
+        //      // Yes, it's a power of 2. Generate mips.
+        //      gl.generateMipmap(gl.TEXTURE_2D);
+        //   } else {
+        //      // No, it's not a power of 2. Turn off mips and set
+        //      // wrapping to clamp to edge
+        //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        //      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        //   }
+    }) as Box<dyn FnMut()>);
+    image.set_onload(Some(callback.as_ref().unchecked_ref()));
     image.set_src(url);
-  
-    return texture;
-  }
 
-  fn is_power_of_2(value: usize) -> bool {
+    callback.forget();
+
+    Ok(texture)
+}
+
+fn is_power_of_2(value: usize) -> bool {
     (value & (value - 1)) == 0
-  }
+}
